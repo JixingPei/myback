941bcd96e30364c175405b7c0fb2d9d3e8ca0ae5
jixing.pei <jixing.pei@sbibits.com>
2020-07-31 10:21:57 +0800

    update

--- src/main/java/com/oldpei/myback/MybackApplication.java
+++ src/main/java/com/oldpei/myback/MybackApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MybackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MybackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MybackApplication.class, args);
+    }
 
 }
