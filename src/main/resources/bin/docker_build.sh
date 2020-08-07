23c3163dc1dc004562f5d2867eb7a7b4386592e5
jixing.pei <jixing.pei@sbibits.com>
2020-08-07 09:09:50 +0800

    docker配置变更

--- src/main/resources/bin/docker_build.sh
+++ src/main/resources/bin/docker_build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-. ./function/docker_function.sh
 
+#compose 安装
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#将可执行权限应用于二进制文件：
+sudo chmod +x /usr/local/bin/docker-compose
+#创建软链：
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#测试是否安装成功：
+docker-compose --version
 
-#myback
-$(run_docker_build './dockerFiles/Dockerfile_back' 'myback' '0.0.1' ) 
-#myfront
-$(run_docker_build './dockerFiles/Dockerfile_front' 'myfront' '0.0.1' ) 
+#运行docker-compose
+docker-compose up
